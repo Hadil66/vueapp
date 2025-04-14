46ca900778f820b4866b23d5a20a7fa57eb4eaeb
Hadil <hadil.ahmed@hva.nl>
2025-04-14 16:48:43 +0200

    test

--- vueapp/components.d.ts
+++ vueapp/components.d.ts
@@ -9,6 +9,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./src/components/AppFooter.vue')['default']
+    Cards: typeof import('./src/components/Cards.vue')['default']
+    Footer: typeof import('./src/components/AppFooter.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
