02acd70bac03b4bfbc9115e39bae1f4964a4e617
Hadil <hadil.ahmed@hva.nl>
2025-04-18 14:33:35 +0200

    version 1 #3
    template dashboard

--- vueapp/src/components/Cards.vue
+++ vueapp/src/components/Cards.vue
@@ -2,54 +2,105 @@
     <v-container>
         <v-row>
             <v-col cols="12" md="4">
-            <v-card class="features">
-                <v-card-item>
-                <v-card-title>Ruimte 1</v-card-title>
+                        <v-card class="features">
+                <v-carousel hide-delimiters>
+                    <v-carousel-item v-for="(ruimte, index) in ruimtes" :key="index">
+                        <div class="container">
+                            <v-card-title>{{ ruimte }}</v-card-title>
+                            <v-card-subtitle>Max aantal personen</v-card-subtitle>  
+                            <v-img
+                                src="https://via.placeholder.com/300x200"
+                                alt="placeholder"
+                                height="200"
+                                class="placeholder-image"
+                            >
+                            </v-img>
+                            <v-card-text> Meer ruimte informatie </v-card-text>
+                        </div>
+                    </v-carousel-item>
+                </v-carousel>
+                            <p> feature 1 </p>
+                            <p> feature 2 </p>
+                            <p> feature 3 </p>
+                            <p> feature 4 </p>
+                        </v-card>
 
-                <v-card-subtitle>Max aantal personen</v-card-subtitle>
-                </v-card-item>
-
-                <v-img class="placeholder-image"> </v-img>
-
-                <v-card-text>
-                    Meer ruimte informatie
-                </v-card-text>
-                <p> feature 1 </p>
-                <p> feature 2 </p>
-                <p> feature 3 </p>
-                <p> feature 4 </p>
-
-            </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
 
-
+<script>
+    export default {
+        data() {
+            return {
+                ruimtes: [
+                    'Ruimte 1',
+                    'Ruimte 2',
+                    'Ruimte 3',
+                    'Ruimte 4',
+                    'Ruimte 5',
+                    'Ruimte 6',
+                    'Ruimte 7'
+                ],
+            };
+        },  
+    };
+</script>
 
 <style>
-    .features {
-        width: 80% !important;
-        margin: 0 auto;
+    .v-window {
+        height: auto !important;
     }
 
+    .v-window__controls {
+        position: relative !important;
+    }
+
+    .v-card {
+        margin: 0 !important;
+        max-width: none !important;
+    }
+    
+    .features { 
+        width: 80% !important;
+        margin: 0 auto !important;
+        padding: 16px  !important;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+    }
     .placeholder-image {
         background-image: url('@/assets/drk-plc-hldr.png');
         background-size: cover;
         background-position: center;
-        height: 8em;
-        width: 5em;
-        border: solid 3px pink; 
-        margin: 0 16px;
+        height: 9em;
+        width: 8em;
+        border: solid 1px pink; 
+    }
+
+    .v-card-text {
+        padding: 16px 0 !important;
     }
 
     p {
         text-align: center;
         padding: 5px;
+        height: 6em;
         border: 1px solid pink;
-        margin: 8px;
         transition: 0.3s;
     }
+
+    @media (max-width: 849px) {
+        p {
+            margin: 16px;
+        }
+    }
+
+
     p:hover {
         -webkit-box-shadow: 2px 2px 16px 0px rgba(148,148,148,0.7);
         cursor: pointer
@@ -59,19 +110,22 @@
         .features {
             display: grid !important;
             grid-template-columns: repeat(3, 1fr) !important;
-            grid-template-rows: 1fr 1fr 0.5fr !important;
+            grid-template-rows: repeat(2, 1fr) !important;
             grid-column-gap: 10px !important;
-            grid-row-gap:10px !important;
+            grid-row-gap: 0px !important;
         }
 
-        .v-card-title { grid-area: 1 / 1 / 2 / 2 !important; }
-        .v-card-subtitle { grid-area: 2 / 1 / 3 / 2 !important; }
-        .placeholder-image { grid-area: 2 / 1 / 3 / 2 !important; }
-        .v-card-text { grid-area: 3 / 1 / 4 / 2 !important; }
-        .p:nth-of-type(1) { grid-area: 1 / 2 / 3 / 3 !important; }
-        .p:nth-of-type(2) { grid-area: 1 / 3 / 3 / 4 !important; }
-        .p:nth-of-type(3) { grid-area: 3 / 2 / 5 / 3 !important; }
-        .p:nth-of-type(4) { grid-area: 3 / 3 / 5 / 4 !important; }
-            
+        .v-carousel { grid-area: 1 / 1 / 3 / 2 !important; }
+        p:nth-of-type(1) { grid-area: 1 / 2 / 2 / 3 !important; margin-top: 1rem !important; }
+        p:nth-of-type(2) { grid-area: 1 / 3 / 2 / 4 !important; margin-top: 1rem !important; }
+        p:nth-of-type(3) { grid-area: 2 / 2 / 3 / 3 !important; }
+        p:nth-of-type(4) { grid-area: 2 / 3 / 3 / 4 !important; }
+    }
+
+    @media (min-width: 960px) {
+        .v-col-md-4 {
+            flex: none !important;
+            max-width: none !important;
+        }
     }
 </style>
--- vueapp/src/components/Dashboard.vue
+++ vueapp/src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="mx-auto" max-width="200">
+    <h1>Locaties in de randstad</h1>
     <v-list>
-      <h1>Locaties in de randstad</h1>
       <v-list-item v-for="(locatie, index) in locaties" :key="index">
         <v-list-item-content>
           <v-list-item-title>{{ locatie }}</v-list-item-title>
@@ -31,20 +31,49 @@ export default {
 
 <style>
   .v-card.mx-auto {
-    max-width: none !important;
-    border-radius: 0 !important;
+    max-width: 960px !important;
+    border-radius: 8px !important;
+    margin: 2em !important;
+    padding: 2em;
   }
 
-  @media (min-width: 600px) { 
-    .v-list {
-      background: none !important;
+  .v-list {
       display: flex !important;
+      flex-direction: column !important;
+      flex-wrap: wrap !important;
+  }
+
+  @media (min-width:600px) {
+    .v-list {
+      height: 15em !important;
+    }
+  }
+
+  @media (min-width: 850px) { 
+    .v-list {
+      height: 10em;
+    }
+  }
+
+  @media (min-width:960px) {
+    .v-card.mx-auto {
+      margin: 2em auto !important;
     }
   }
 
   h1 {
     font-size: clamp(1.5625rem, 1.027rem + 2.2549vw, 3rem) !important;
     padding: 0 16px;
+    height: 4em;
+    width: clamp(200px, 40%, 400px);
+  }
+
+  .v-list-item {
+    height: 2em;
+  }
+
+  .v-list-item-title {
+    font-size: clamp(1.2rem, -2.2593rem + 377.9341vw, 1.5rem) !important;
   }
 
   .v-list-item:hover, .v-list-item:focus {
